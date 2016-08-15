667b7b73994a25519b7cb7b769efb97581abd4be
Joseph Ellis <jhellis3@gmail.com>
2016-08-15 16:17:30 -0500

    Code aesthetics

--- src/search.cpp
+++ src/search.cpp
@@ -647,7 +647,7 @@ namespace {
     // Step 4a. Tablebase probe
     if (!rootNode && TB::Cardinality)
     {
-        int piecesCnt = pos.count<ALL_PIECES>(WHITE) + pos.count<ALL_PIECES>(BLACK);
+        int piecesCnt = popcount(pos.pieces());
 
         if (    piecesCnt <= TB::Cardinality
             && (piecesCnt <  TB::Cardinality || depth >= TB::ProbeDepth)
@@ -663,7 +663,9 @@ namespace {
                 int drawScore = TB::UseRule50 ? 1 : 0;
 
                 if (    abs(v) <= drawScore
-                    || (!ttHit || ((v < -drawScore && ttValue > -VALUE_KNOWN_WIN) || (v > drawScore && ttValue < VALUE_KNOWN_WIN))))
+                    || !ttHit
+                    || (v < -drawScore && ttValue > -VALUE_KNOWN_WIN)
+                    || (v >  drawScore && ttValue <  VALUE_KNOWN_WIN))
                 {
                     value =  v < -drawScore ? -VALUE_MATE_IN_MAX_PLY + ss->ply + (pos.non_pawn_material(pos.side_to_move()) - pos.non_pawn_material(~pos.side_to_move())) / 256
                            : v >  drawScore ?  VALUE_MATE_IN_MAX_PLY - ss->ply + (pos.non_pawn_material(pos.side_to_move()) - pos.non_pawn_material(~pos.side_to_move())) / 256
