2b7c6971be4da6f942a433fd832f976cb1bb99c5
ChessMan3 <amult@ukr.net>
2017-08-04 13:50:42 +0300

    Saving Hash

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -21,6 +21,7 @@
 #include <algorithm>
 #include <cassert>
 #include <ostream>
+#include <iostream>
 
 #include "misc.h"
 #include "search.h"
@@ -30,6 +31,7 @@
 #include "syzygy/tbprobe.h"
 
 using std::string;
+using namespace std;
 
 UCI::OptionsMap Options; // Global object
 
@@ -42,6 +44,9 @@ void on_large_pages(const Option& o) { TT.resize(o); }  // warning is ok, will b
 void on_logger(const Option& o) { start_logger(o); }
 void on_threads(const Option&) { Threads.read_uci_options(); }
 void on_tb_path(const Option& o) { Tablebases::init(o); }
+void on_HashFile(const Option& o) { TT.set_hash_file_name(o); }
+void SaveHashtoFile(const Option&) { TT.save(); }
+void LoadHashfromFile(const Option&) { TT.load(); }
 
 
 /// Our case insensitive less() function as required by UCI protocol
@@ -76,6 +81,10 @@ void init(OptionsMap& o) {
   o["SyzygyProbeDepth"]      << Option(1, 1, 100);
   o["Syzygy50MoveRule"]      << Option(true);
   o["SyzygyProbeLimit"]      << Option(6, 0, 6);
+  o["NeverClearHash"]		 << Option(false);
+  o["HashFile"]				 << Option("hash.hsh", on_HashFile);
+  o["SaveHashtoFile"]		 << Option(SaveHashtoFile);
+  o["LoadHashfromFile"]		 << Option(LoadHashfromFile);
 }
 
 
